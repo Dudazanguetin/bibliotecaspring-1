42a1cedc778abad959fd7744b19fb10d12ada486
Francieli Martins Rocha <119825620+FrancieliMrtns@users.noreply.github.com>
2023-05-18 21:36:58 -0300

    arrumando

--- app/src/main/java/application/model/Livro.java
+++ app/src/main/java/application/model/Livro.java
@@ -13,19 +13,24 @@ public class Livro {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     private String titulo;
+    private String isbn;
 
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
     public int getId() {
         return id;
     }
     public void setId(int id) {
         this.id = id;
     }
-    
-
     public String getTitulo() {
         return titulo;
     }
     public void setTitulo(String titulo) {
         this.titulo = titulo;
-    }
+    }   
 }
